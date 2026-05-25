3c6c487e4562b93e8de841c038d0d504aa1fdc49
cheolmin880 <kimcm24@gmail.com>
2026-05-25 16:31:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE_VERSION = 'v20260525';
+const CACHE_NAME = 'golf-app-' + CACHE_VERSION;
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
